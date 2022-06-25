117c07e09a63015a40782059c9da0f4f06ddae31
Gustavo Damasceno de Campos <gdcampos@solarbr.com.br>
2022-06-25 15:02:00 -0300

    front-end

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,13 +1,21 @@
 import './style.css'
 
 import logo from '../../Images/logo.svg'
+import discover from '../../Images/discover.svg'
+import config from '../../Images/config.svg'
+import info from '../../Images/info.svg'
+import duvida from '../../Images/duvida.svg'
 
 function Navbar() {
     return(
         <div className="navcontain">
             <img className='logoimg' src={logo} />
             <p className='tittlebar'>Google Meet</p>
-
+            
+            <img className='duvida' src={duvida} />
+            <img className='info' src={info} />
+            <img className='config' src={config} />
+            <img className='disco' src={discover} />
             <div className='perfil'></div>
         </div>
     )

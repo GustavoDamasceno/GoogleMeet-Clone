30752d0b5c9ef5059440ee1bcb0f37e6d463eace
Gustavo Damasceno de Campos <gdcampos@solarbr.com.br>
2022-06-25 17:47:01 -0300

    front-end

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './style.css'
 
 import logo from '../../Images/logo.svg'
@@ -6,12 +7,20 @@ import config from '../../Images/config.svg'
 import info from '../../Images/info.svg'
 import duvida from '../../Images/duvida.svg'
 
+
 function Navbar() {
+    const hora = 17;
+    const minuto = 45;
+
     return(
         <div className="navcontain">
             <img className='logoimg' src={logo} />
             <p className='tittlebar'>Google Meet</p>
-            
+
+            <p className='txthours'>{hora}:{minuto}</p>
+            <p className='point'>•</p>
+            <p className='data'>Mon, 21 Feb</p>
+
             <img className='duvida' src={duvida} />
             <img className='info' src={info} />
             <img className='config' src={config} />

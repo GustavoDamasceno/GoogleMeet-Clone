01af546f6b66b0a802ec6c930b8d97a6a0615c46
Gustavo Damasceno de Campos <gdcampos@solarbr.com.br>
2022-06-25 23:15:02 -0300

    front-end

--- src/Components/Bg/index.jsx
+++ src/Components/Bg/index.jsx
@@ -2,7 +2,16 @@ import './style.css'
 
 function Bg() {
     return(
-        <div className='bgcontain'></div>
+        <div className='bgcontain'>
+            <h3>Premium video meetings. Now free for everyone.</h3>
+            <p className='txtbg'>We re-engineered the service that we built for secure business meetings, Google Meet, to make it free and available for all.</p>
+            <div className='blocoinput'>
+                <button>New meeting</button>
+                <input placeholder='Enter a code or link'></input>
+            </div>
+            <hr/>
+            <p className='endbg'><span>Learn more</span> about Google Meet</p>
+        </div>
     )
 }
 
